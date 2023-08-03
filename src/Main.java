95a482427003c9082e2b100e04c007746ad098fb
MehmetKurmaz <111184509+MehmetKurmaz@users.noreply.github.com>
2023-08-03 10:28:43 +0300

    added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int nsayi,result=0;
+        System.out.print("Sayi Giriniz :");
+        Scanner scanner=new Scanner(System.in);
+        nsayi=scanner.nextInt();
+        for (int i=1;nsayi>i;i++){
+            if (nsayi%i==0){
+               result=result+i;
+            }
+
+        }
+        if (nsayi==result){
+            System.out.println(nsayi+ " Mukemmel sayidir.");
+        }else {
+            System.out.println(nsayi + " Mukemmel sayi Degildir.");
+        }
     }
 }
